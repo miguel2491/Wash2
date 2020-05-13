22dffa0b523874c8f9e50ec607ad6b818280537f
Miguel <miguelin2491@gmail.com>
2020-05-12 23:28:46 -0500

    Correccion Mapa
    Mapa Geolocalizador

--- Wash2/Wash2/Views/Estado/Confirmar.xaml.cs
+++ Wash2/Wash2/Views/Estado/Confirmar.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Plugin.Geolocator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,9 @@ using System.Threading.Tasks;
 using Wash2.Models;
 using Wash2.Views.Solicitudes;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using System.Threading;
 
 namespace Wash2.Views.Estado
 {
@@ -19,8 +22,85 @@ namespace Wash2.Views.Estado
 		public Confirmar (int id_s)
 		{
 			InitializeComponent ();
+            _ = CurrentLocation();
             idS = id_s;
-		}
+            Device.StartTimer(TimeSpan.FromMinutes(1), () => //Will start after 1 min
+            {
+                Task.Run(() =>
+                {
+                    //Mapx.IsVisible = false;
+                    Console.WriteLine("CADA MINUTE");
+                    // do something with time...
+                    
+                });
+
+                return false; // To repeat timer,always return true.If you want to stop the timer,return false
+            });
+        }
+
+        public async Task CurrentLocation()
+        {
+            
+            Mapx.Pins.Clear();
+            Mapx.IsVisible = true;
+            var pos = await CrossGeolocator.Current.GetPositionAsync();
+
+            Mapx.MoveToRegion(
+            MapSpan.FromCenterAndRadius(
+            new Position(pos.Latitude, pos.Longitude), Distance.FromMiles(1)));
+            HttpClient client = new HttpClient();
+            var url = "http://www.washdryapp.com/app/public/solicitud/lista_solicitud/" + idS;
+            try
+            {
+                var response = await client.GetAsync(url);
+                switch (response.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        Console.WriteLine("----------------------------------------------_____:Here status 500");
+                        break;
+                    case System.Net.HttpStatusCode.OK:
+                        Console.WriteLine("----------------------------------------------_____:Here status 200");
+                        HttpContent content = response.Content;
+                        var xjson = await content.ReadAsStringAsync();
+                        var json_ = JsonConvert.DeserializeObject<List<Solicitud>>(xjson);
+                        var lat = Convert.ToDouble(json_[0].latitud);
+                        var lon = Convert.ToDouble(json_[0].longitud);
+                        //lat = Convert.ToDecimal(lat);
+                        var pin = new Pin
+                        {
+                            Type = PinType.Place,
+                            Position = new Position(lon, lat),
+                            Label = "Mi ubicacion",
+                            Address = "  usted se encuentra aqui",
+
+                        };
+                        Mapx.Pins.Add(pin);
+                        /*RUTA DRAW*/
+                        Map map = new Map { };
+                        Polyline polyline = new Polyline
+                        {
+                            StrokeColor = Color.Blue,
+                            StrokeWidth = 12,
+                            Geopath =
+                            {
+                                new Position(pos.Latitude, pos.Longitude),
+                                new Position(lon, lat)
+                            }
+                        };
+                        Mapx.MapElements.Add(polyline);
+                        //
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("", "" + ex.ToString(), "ok");
+                return;
+            }
+
+        }
+
+
 
         private async void BtnConfirma_Clicked(object sender, EventArgs e)
         {
